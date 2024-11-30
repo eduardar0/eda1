2925fc0750b215bff2b2c9138b39d29bdb8a48a1
Eduarda Rodrigues <eduardarodrigues151108@gmail.com>
2024-11-29 21:48:10 -0300

    quick-MOJ

--- ordenacao/sememnte.c
+++ ordenacao/sememnte.c
@@ -5,6 +5,7 @@ typedef struct {
     long long S;  // código da semente
     int N;        // nota da semente
 } Semente;
+
 void exch(void *a, void *b){
     Semente *x = (Semente *) a;
     Semente *y = (Semente *) b;
@@ -12,6 +13,7 @@ void exch(void *a, void *b){
     *x = *y; 
     *y = temp;
 }
+
 int partition(Semente *v, int l, int r){
     int i = l-1, j = r;
     Semente pivo = v[r];
@@ -19,41 +21,57 @@ int partition(Semente *v, int l, int r){
     while(i<j){
         while ((v[++i].S < pivo.S) || (v[i].S == pivo.S && v[i].N < pivo.N));
 
-        // Retrocede o índice 'j' enquanto os elementos estão na ordem correta
         while (j > l && ((v[--j].S > pivo.S) || (v[j].S == pivo.S && v[j].N > pivo.N)));
 
-        if(i<j) exch(&v[i],&v[j]); 
-        //desempate
-
-        // while (j > l && ((v[--j].S > pivo.S) || (v[j].S == pivo.S && v[j].N > pivo.N)));
-
+        if(i<j) exch(&v[i],&v[j]);
     }
     exch(&v[i], &v[r]); 
     return i; 
 }
 
 void quick_sort(Semente *v, int l, int r){
-    if(r<=l) return; 
+    if(r<=l) return;
 
     int p = partition(v, l, r);
     quick_sort(v, l, p-1);
     quick_sort(v, p+1, r);
-
 }
 
 int main() {
-    Semente v[] = {{10, 90}, {20, 85}, {15, 88}, {5, 80}, {30, 80}};
+    int k;
+    scanf("%d", &k);
 
-    int r =sizeof(v) / sizeof(v[0]);
+    int i = 0;
+    Semente *v = malloc(1000 * sizeof(Semente)); // Ajuste no tamanho da alocação
 
-    quick_sort(v, 0, r-1); 
-
-    int k =4;
-
-    for(int i =0; i<k; i++){
-        printf("Semente com menor código: S = %lld, N = %d\n", v[i].S, v[i].N);
+    if (v == NULL) {
+        fprintf(stderr, "Erro de alocação de memória.\n");
+        return 1;
     }
 
+    // Lê as sementes até o fim do arquivo (EOF)
+    while (scanf("%lld %d", &v[i].S, &v[i].N) != EOF) {
+        i++;
+        if (i % 1000 == 0) {  // Realoque mais memória conforme necessário
+            v = realloc(v, (i + 1000) * sizeof(Semente));
+            if (v == NULL) {
+                fprintf(stderr, "Erro de alocação de memória.\n");
+                return 1;
+            }
+        }
+    }
+
+    int r = i;
+
+    // Ordenação das sementes
+    quick_sort(v, 0, r-1);
+
+    // Imprime as k sementes com menor código
+    for (int i = 0; i < k; i++) {
+        printf("\nSemente com menor código: S = %lld, N = %d\n", v[i].S, v[i].N);
+    }
+
+    free(v);
 
     return 0;
-} 
+}
