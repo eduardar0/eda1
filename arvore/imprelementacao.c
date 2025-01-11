d76e54ef72b65cf53a13bd56562097d879904c89
Eduarda Rodrigues <eduardarodrigues151108@gmail.com>
2025-01-10 23:22:40 -0300

    implementação de arvore versão recursiva

--- arvore/imprelementacao.c
+++ arvore/imprelementacao.c
@@ -1,31 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 // Define o tipo de item que será armazenado nos nós da árvore
 typedef int Item;
 
-// Declaração da estrutura do nó da árvore
-typedef struct node {
-    Item item;        // Valor armazenado no nó
-    struct node *pai; // Ponteiro para o nó pai
-    struct node *esq; // Ponteiro para o filho esquerdo
-    struct node *dir; // Ponteiro para o filho direito
-} no;
+typedef struct No{
+    Item item;
+    No *pai;
+    No *esq, *dir;
 
-// Função para criar uma árvore binária com um nó raiz
-no *criar_arvore(Item x, no *p, no *e, no *d) {
-    // Aloca memória para o nó raiz
-    no *raiz = (no *)malloc(sizeof(no));
-    if (raiz == NULL) {
-        printf("Erro de alocação de memória!\n");
-        exit(1);
+}No;
+
+No *criar_arvore(Item x, No *pai, No* esq, No *dir){
+    No *raiz = malloc(sizeof(No));
+    raiz->pai = pai;
+    raiz->esq = esq;
+    raiz->dir = dir;
+    raiz->item = x;
+    return raiz;
+}
+
+No *avo(No *elemento){
+    if(elemento != NULL && elemento->pai != NULL) return elemento->pai->pai;
+
+    return NULL;
+
+}
+
+No *tio(No *elemento){
+    No *vovo = avo(elemento);
+    if(vovo == NULL) return NULL;
+    if(vovo->dir == elemento->pai) return vovo->esq;
+
+    return vovo->dir;
+}
+
+No *irmao(No *elemento){
+    if((elemento != NULL) && (elemento->pai != NULL)){
+        if(elemento->pai->esq == elemento) return elemento->pai->dir;
+        else return elemento->pai->esq;
     }
 
-    // Inicializa os campos do nó raiz
-    raiz->item = x;    // Atribui o valor ao nó
-    raiz->pai = p;     // Define o nó pai
-    raiz->esq = e;     // Define o filho esquerdo
-    raiz->dir = d;     // Define o filho direito
-
-    return raiz;       // Retorna o ponteiro para o nó raiz
+    return NULL;
 }
+
+No *busca_linear(No *elemento, Item x){
+
+    if(elemento ==NULL || elemento->item == x) return elemento; 
+
+    No *busca_esquerda = busca_linear(elemento->esq, x);
+    if(busca_esquerda!=NULL) return busca_esquerda;
+
+    return busca_linear(elemento->dir, x);
+
+}
+
+int numero_nos(No *elemento){
+    if(elemento == NULL) return 0; 
+
+    return 1 + numero_nos(elemento->esq) + numero_nos(elemento->dir); 
+}
+
+int altura(No *elemento){
+    if(elemento==NULL) return 0; 
+    int h_esq = 1+ altura(elemento->esq);
+    int h_dir= 1 + altura(elemento->dir);
+
+    return 1 + ( h_esq>h_dir ? h_esq : h_dir);
+}
+
+
